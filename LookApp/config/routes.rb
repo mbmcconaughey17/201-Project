16837de6d1b7fe2ebb9389e9edfffab49db917e2
mcconamb <mcconamb@miamioh.edu>
2015-12-01 09:40:32 -0500

    Playing around with Rating system

--- LookApp/config/routes.rb
+++ LookApp/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  post '/rate' => 'rater#create', :as => 'rate'
+  #post '/rate' => 'rater#create', :as => 'rate'
   resources :apps do
       resources :ratings
   end
